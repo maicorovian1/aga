b2a4e62d7e0352acc5315cf3b1cb82156bddeefa
maicorovian1 <154009277+maicorovian1@users.noreply.github.com>
2025-05-21 19:59:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -30,4 +30,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
   showSlide(slideIndex);
 });
+let slideIndex = 0;
+const slidesDesktop = document.querySelectorAll(".slides-desktop .slide");
+const slidesMobile = document.querySelectorAll(".slides-mobile .slide");
 
+function showSlide(index) {
+  const isMobile = window.innerWidth <= 768;
+  const slides = isMobile ? slidesMobile : slidesDesktop;
+
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) slide.classList.add("active");
+  });
+}
+
+function changeSlide(step) {
+  const isMobile = window.innerWidth <= 768;
+  const slides = isMobile ? slidesMobile : slidesDesktop;
+
+  slideIndex = (slideIndex + step + slides.length) % slides.length;
+  showSlide(slideIndex);
+}
+
+setInterval(() => {
+  changeSlide(1);
+}, 5000); // troca a cada 5 segundos
+
+window.addEventListener("resize", () => {
+  showSlide(slideIndex);
+});
+
+showSlide(slideIndex); // exibir slide inicial
